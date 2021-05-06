ce2ec284a7e0c3baf51bcd5f915e6f802806594c
XXXuanHuang <45673070+XXXuanHuang@users.noreply.github.com>
2021-05-06 15:17:05 +0800

    print metrics output for ML models

---
+++ evaluation.py
@@ -0,0 +1,8 @@
+'''This file prints result of evaluation metrics'''
+from sklearn.metrics import plot_roc_curve, accuracy_score, confusion_matrix, f1_score, balanced_accuracy_score, roc_auc_score
+
+def binary_eval(model_type:str, label, prediction):
+    print(model_type,'accuracy', balanced_accuracy_score(label, prediction))
+    print(model_type,'forest f1',f1_score(label, prediction, average='weighted'))
+    print(model_type,'roc_auc', roc_auc_score(label, prediction, average='weighted'))
+    print(model_type,'confusion matrix', confusion_matrix(label, prediction))
